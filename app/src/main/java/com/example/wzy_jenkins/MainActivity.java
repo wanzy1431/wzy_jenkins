bf441c2bc8e5ad62f8d369a28a0a7b39dff2d4e2
wanzhongyi <wan_zhong_yi@163.com>
2020-09-17 17:02:28 +0800

    Initial commit

---
+++ app/src/main/java/com/example/wzy_jenkins/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.wzy_jenkins;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
